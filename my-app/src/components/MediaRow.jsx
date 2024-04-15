1e944dfce01d08cb07ff89a7f8516cfda870e763
Jafestro <jafestro@gmail.com>
2024-04-15 15:28:31 +0300

    add tailwind

--- my-app/src/components/MediaRow.jsx
+++ my-app/src/components/MediaRow.jsx
@@ -1,28 +1,29 @@
 import PropTypes from "prop-types";
+import Button from "./UI/Button";
 
 const MediaRow = (props) => {
-  const {item, setSelectedItem} = props;
+  const { item, setSelectedItem } = props;
   const handleClick = () => setSelectedItem(item);
   return (
     <tr>
       <td>
-        <img src={item.thumbnail} alt={item.title}/>
+        <img src={item.thumbnail} alt={item.title} />
       </td>
       <td>{item.title}</td>
       <td>{item.description}</td>
       <td>{new Date(item.created_at).toLocaleString()}</td>
       <td>{item.filesize}</td>
       <td>{item.media_type}</td>
-        <td>
-            <button onClick={handleClick}>View</button>
-        </td>
+      <td>
+        <Button text={"View"} handleClick={handleClick} />
+      </td>
     </tr>
   );
 };
 
 MediaRow.propTypes = {
   item: PropTypes.object.isRequired,
-    setSelectedItem: PropTypes.func.isRequired,
+  setSelectedItem: PropTypes.func.isRequired,
 };
 
 export default MediaRow;
--- my-app/src/components/SingleView.jsx
+++ my-app/src/components/SingleView.jsx
@@ -1,20 +1,34 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import Button from "./UI/Button.jsx";
 
-const SingleView = props => {
-  const {selectedItem, setSelectedItem} = props;
+const SingleView = (props) => {
+  const { selectedItem, setSelectedItem } = props;
   const handleClick = () => setSelectedItem(null);
   return (
-    <dialog open={selectedItem ? true : false}>
+    <dialog
+      className={
+        "fixed top-0 h-dvh w-dvw bg-black bg-opacity-50 text-stone-100 "
+      }
+      open={selectedItem ? true : false}
+    >
       <p>
-        <button onClick={handleClick}>Close</button>
+        <Button text={"Close"} handleClick={handleClick} />
       </p>
       {selectedItem && (
         <>
-          {selectedItem.media_type.includes('video') ? (
-            <video controls>
-              <source src={selectedItem.filename} type={selectedItem.media_type}/>
-            </video>) : (
-            <img src={selectedItem.filename} alt={selectedItem.title}/>
+          {selectedItem.media_type.includes("video") ? (
+            <video className={"h-3/4 m-auto"} controls>
+              <source
+                src={selectedItem.filename}
+                type={selectedItem.media_type}
+              />
+            </video>
+          ) : (
+            <img
+              className={"h-3/4 m-auto"}
+              src={selectedItem.filename}
+              alt={selectedItem.title}
+            />
           )}
           <h2>{selectedItem.title}</h2>
           <p>{selectedItem.description}</p>
