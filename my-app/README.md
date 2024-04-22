4c0eea875296cff2f752a22d5957c007d6949868
Jafestro <jafestro@gmail.com>
2024-04-22 13:04:50 +0300

    add custom-hooks build the app

--- my-app/README.md
+++ my-app/README.md
@@ -1,4 +1,4 @@
-# WSK-Hooks
+# WSK-Custom-Hooks
 Week6
 
-Open [link text here](https://users.metropolia.fi/~jafarj/wsk/wsk-hooks/) to view it in the browser.
+Open [link text here](https://users.metropolia.fi/~jafarj/wsk/wsk-custom-hooks/) to view it in the browser.
--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,34 +1,11 @@
 import MediaRow from "./MediaRow.jsx";
-import { useEffect, useState } from "react";
-import { fetchData } from "../lib/fetchData.js";
+import { useMedia } from "../hooks/ApiHooks.js";
 
 const Home = () => {
   // const [selectedItem, setSelectedItem] = useState(null);
-  const [mediaArray, setMediaArray] = useState([]);
+  // const mediaArray = useMedia().mediaArray; or like that
+  const { mediaArray } = useMedia();
 
-  const getMedia = async () => {
-    const mediaResult = await fetchData(
-      import.meta.env.VITE_MEDIA_API + "/media",
-    );
-
-    const mediaWithUser = await Promise.all(
-      mediaResult.map(async (mediaItem) => {
-        const userResult = await fetchData(
-          import.meta.env.VITE_AUTH_API + "/users/" + mediaItem.user_id,
-        );
-        return { ...mediaItem, username: userResult.username };
-      }),
-    );
-
-    console.log(mediaWithUser);
-    setMediaArray(mediaWithUser);
-  };
-
-  useEffect(() => {
-    getMedia();
-  }, []);
-
-  console.log(mediaArray);
   return (
     <>
       <h2>My Media</h2>
